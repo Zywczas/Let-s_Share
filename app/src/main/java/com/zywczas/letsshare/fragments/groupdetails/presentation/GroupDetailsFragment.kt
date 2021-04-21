b95d3b80e59f5af28c5d43e7816d20b58091dd1a
PiotrZywczewski <zywczas@o2.pl>
2021-04-21 19:56:12 +0200

    list_item_group_members.xml have stroke_background.xml

--- app/src/main/java/com/zywczas/letsshare/fragments/groupdetails/presentation/GroupDetailsFragment.kt
+++ app/src/main/java/com/zywczas/letsshare/fragments/groupdetails/presentation/GroupDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,6 +13,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.leinardi.android.speeddial.SpeedDialActionItem
 import com.leinardi.android.speeddial.SpeedDialView
 import com.zywczas.letsshare.R
