7c5853962bab0591b427a0e192bbff62ceb4e665
PiotrZywczewski <zywczas@o2.pl>
2021-06-11 19:47:22 +0200

    refactoring

--- app/src/test/java/com/zywczas/letsshare/fragments/groupdetails/presentation/GroupDetailsFragmentTest.kt
+++ app/src/test/java/com/zywczas/letsshare/fragments/groupdetails/presentation/GroupDetailsFragmentTest.kt
@@ -12,38 +12,9 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class GroupDetailsFragmentTest {
 
-    var i = 1 //todo sprawdzic czy za kazdym razem jest nowa instancja klasy
-
     @get:Rule
     val testCoroutine = TestCoroutineRule()
     @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule().also {
-        println(i)
-        i++
-    }
-
-    @Test
-    fun test1(){
-
-    }
-    @Test
-    fun test5(){
-
-    }
-
-    @Test
-    fun test4(){
-
-    }
-
-    @Test
-    fun test3(){
-
-    }
-
-    @Test
-    fun test2(){
-
-    }
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
 }
