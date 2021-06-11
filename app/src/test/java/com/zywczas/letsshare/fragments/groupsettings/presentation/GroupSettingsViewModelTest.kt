e0007dbc1cc5af171f88094002759b27e923da12
PiotrZywczewski <zywczas@o2.pl>
2021-06-11 20:04:22 +0200

    1 test in GroupSettingsViewModelTest.kt

--- app/src/test/java/com/zywczas/letsshare/fragments/groupsettings/presentation/GroupSettingsViewModelTest.kt
+++ app/src/test/java/com/zywczas/letsshare/fragments/groupsettings/presentation/GroupSettingsViewModelTest.kt
@@ -2,10 +2,15 @@ package com.zywczas.letsshare.fragments.groupsettings.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import com.zywczas.letsshare.SessionManager
 import com.zywczas.letsshare.fragments.groupsettings.domain.GroupSettingsRepository
+import com.zywczas.letsshare.models.Friend
 import com.zywczas.letsshare.testrules.TestCoroutineRule
+import com.zywczas.letsshare.utils.LiveDataTestUtil
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,7 +28,14 @@ class GroupSettingsViewModelTest {
 
     @Test
     fun getFriendsShouldGetFriends() = coroutineTest.runBlockingTest {
+        val expected = listOf(Friend("id", "email", "name"))
+        whenever(repository.getFriends()).thenReturn(expected)
 
+        tested.getFriends()
+        val actual = LiveDataTestUtil.getValue(tested.friends)
+
+        verify(repository).getFriends()
+        assertThat(actual).isEqualTo(expected)
     }
 
 }
